e76ab8a7bcb740c8a9add3c276955230a1b647cc
Ahmet Novalic <ahmet@Ahmets-MacBook-Pro.local>
2016-11-22 20:27:29 +0000

    Test commit, change to local mongo instance for sanity

--- app/app.py
+++ app/app.py
@@ -5,11 +5,16 @@ app = Flask(__name__)
 
 #load the config from pyfile
 app.config.from_envvar('DSFOOD_CONFIG')
-client = MongoClient(app.config['MONGO_URI'])
+#run it locally for now on
+client = MongoClient()
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+	db = client.test_database
+	posts = db.posts
+	post = {'author':'ahmet the man', 'text': 'This is just a sample text, I wonder where she is btw!'}
+	post_id = posts.insert_one(post)
+	return str(post_id)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=8080)
