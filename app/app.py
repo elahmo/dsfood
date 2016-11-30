1f636e48c513742203b6e8d3d53c7cfbbaef18a6
distropia <salgadd@gmail.com>
2016-11-30 13:54:25 +0000

    [Mod] [app] Show collection (local env)

--- app/app.py
+++ app/app.py
@@ -11,11 +11,11 @@ client = MongoClient('mongodb://localhost:27017/test')
 @app.route('/')
 def hello_world():
 	db = client.test.collection
-	data = db.find()
+	#data = db.find()
 	#posts = db.posts
 	#post = {'author':'ahmet the man', 'text': 'This is just a sample text, I wonder where she is btw!'}
 	#post_id = posts.insert_one(post)
-	return str(data)
+	return str(db)
 
 if __name__ == '__main__':
     app.run(port=8080)
