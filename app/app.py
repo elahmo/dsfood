9b36def1515e71368329aa3451db773fb848a949
Unknown <salgadd@gmail.com>
2016-11-30 13:29:02 +0000

    [Mod] [app]
    fix test data

--- app/app.py
+++ app/app.py
@@ -6,15 +6,16 @@ app = Flask(__name__)
 #load the config from pyfile
 #app.config.from_envvar('DSFOOD_CONFIG')
 #run it locally for now on
-client = MongoClient()
+client = MongoClient('mongodb://localhost:27017/test')
 
 @app.route('/')
 def hello_world():
-	db = client.test_database
-	posts = db.posts
-	post = {'author':'ahmet the man', 'text': 'This is just a sample text, I wonder where she is btw!'}
-	post_id = posts.insert_one(post)
-	return str(post_id)
+	db = client.test.collection
+	data = db.find()
+	#posts = db.posts
+	#post = {'author':'ahmet the man', 'text': 'This is just a sample text, I wonder where she is btw!'}
+	#post_id = posts.insert_one(post)
+	return str(data)
 
 if __name__ == '__main__':
     app.run(port=8080)
