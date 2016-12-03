4de9d17d898fcc713ceb29fed879aed425799e97
Unknown <salgadd@gmail.com>
2016-12-03 01:08:51 +0000

    [Mod][app] adding secret key

--- app/app.py
+++ app/app.py
@@ -110,5 +110,8 @@ def logout():
     flash('You were logged out')
     return redirect(url_for('main_page'))
 
+# set the secret key.  keep this really secret:
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+
 if __name__ == '__main__':
     app.run(port=8080)
