82b1a251611fedc16e2ddee77ddbd747992dc0a4
shargaas <gtn1n16@soton.ac.uk>
2016-12-15 19:05:53 +0000

    [datasets+script][Add+mod] Transposed csv and tried to use script

--- scripts/csvimporter.py
+++ scripts/csvimporter.py
@@ -10,7 +10,7 @@ client = MongoClient('localhost', 27017)
 db = client.dsfood
 
 #goes through all of the files in the datasets directory
-csv_files = [y for x in os.walk('../datasets/Health') for y in glob(os.path.join(x[0], '*.csv'))]
+csv_files = [y for x in os.walk('../datasets/') for y in glob(os.path.join(x[0], '*.csv'))]
 
 def filename_helper(path):
     head, tail = ntpath.split(path)
@@ -19,7 +19,7 @@ def filename_helper(path):
 #do the actual insertion
 for csv_file in csv_files:
 	try:
-		print csv_file
+		#print csv_file
 		file_path = csv_file
 		dataset = filename_helper(csv_file)
 		delimiter = ',' #change this if your dataset uses something else
@@ -35,7 +35,7 @@ for csv_file in csv_files:
 		results = []
 		for each in reader:
 			results.append(each)
-		print(results)
+		#print(results)
 		collection.insert_many(results)
 		dataset_list.insert_one({'name': dataset})
 	except Exception as e:
