078c339dfb1516e2f148769eb107551fc2e23398
elahmo <ahmetnovalic@gmail.com>
2016-12-16 01:57:00 +0000

    Updated gitignore

--- scripts/csvimporter.py
+++ scripts/csvimporter.py
@@ -10,7 +10,7 @@ client = MongoClient('localhost', 27017)
 db = client.dsfood
 
 #goes through all of the files in the datasets directory
-csv_files = [y for x in os.walk('../datasets/') for y in glob(os.path.join(x[0], '*.csv'))]
+csv_files = [y for x in os.walk('../datasets') for y in glob(os.path.join(x[0], '*.csv'))]
 
 def filename_helper(path):
     head, tail = ntpath.split(path)
@@ -23,6 +23,8 @@ for csv_file in csv_files:
 		file_path = csv_file
 		dataset = filename_helper(csv_file)
 		delimiter = ',' #change this if your dataset uses something else
+		if 'Sugar prices' in file_path or 'US Dollar Index' in file_path:
+			delimiter = ';'
 		dataset_list = db['dataset_list']
 		collection = db[dataset]
 
@@ -39,4 +41,5 @@ for csv_file in csv_files:
 		collection.insert_many(results)
 		dataset_list.insert_one({'name': dataset})
 	except Exception as e:
+		print csv_file
 		raise e
